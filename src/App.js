8ef5383cad204f8e24e55092bca75064f4a133cb
Christian <ch.xxii@gmail.com>
2019-11-02 00:27:56 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+const App = () => {
+  const [saludoVisible, setSaludoVisible] = useState(true);
+  const [nombreActual, setNombreActual] = useState("");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        fontFamily: '"Palatino Linotype", "Book Antiqua", Palatino, serif',
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center"
+      }}
+    >
+      <div>
+        {saludoVisible && <Saludo nombre={nombreActual} />}
+        <input
+          type="text"
+          placeholder="Ingresá tu nombre"
+          value={nombreActual}
+          onChange={e => setNombreActual(e.target.value)}
+        />
+        <button onClick={() => setSaludoVisible(!saludoVisible)}>
+          {saludoVisible ? "Ocultar" : "Mostrar"}
+        </button>
+      </div>
+      <Title />
+      <MenuItems />
     </div>
   );
-}
+};
+
+const Title = () => {
+  return <h1>Coffee House</h1>;
+};
+
+const MenuItems = () => {
+  return (
+    <ul>
+      <Item name="Coffee" color={"#208FD8"} />
+      <Item name="Tea" color={"#EF233C"} />
+      <Item name="Milk" color={"#D5D820"} />
+      <Item name="Other" color={"#000"} />
+    </ul>
+  );
+};
+
+const Item = ({ name, color }) => {
+  return <li style={{ color, fontSize: 18, fontWeight: "bold" }}>{name}</li>;
+};
+
+const Saludo = ({ nombre }) => {
+  const value = nombre === "" ? "<Nombre>" : nombre;
+
+  useEffect(() => {
+    const timer = setTimeout(() => (document.title = `Hola, ${value}`), 3000);
+    return function cleanup() {
+      clearTimeout(timer);
+    };
+  });
+  return <div>Hola, {value}</div>;
+};
 
 export default App;
